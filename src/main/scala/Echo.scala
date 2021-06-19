31ea92e8ce5217c0c5baefb7806feef1acb119f2
Guruprasad Venkatesh <dvguruprasad@Guruprasads-MacBook-Pro.local>
2021-06-19 12:03:11 +0530

    Echo works!

--- src/main/scala/Echo.scala
+++ src/main/scala/Echo.scala
@@ -11,6 +11,11 @@ object Echo extends App {
   @JsonCodec case class InitMessageResponse(src: String, dest: String, body: InitMessageResponseBody)
   @JsonCodec case class InitMessageResponseBody(`type`: String, in_reply_to: Int, msg_id: Int)
 
+  @JsonCodec case class EchoMessage(src: String, dest: String, body: EchoMessageBody)
+  @JsonCodec case class EchoMessageBody(`type`: String, echo: String, msg_id: Int)
+  @JsonCodec case class EchoMessageResponse(src: String, dest: String, body: EchoMessageResponseBody)
+  @JsonCodec case class EchoMessageResponseBody(`type`: String, in_reply_to: Int, msg_id: Int, echo: String)
+
   implicit val decodeInitMessageBody: Decoder[InitMessageBody] = deriveDecoder[InitMessageBody]
   implicit val decodeInitMessage: Decoder[InitMessage] = deriveDecoder[InitMessage]
 
@@ -20,23 +25,31 @@ object Echo extends App {
   var str = readLine()
   var messageId = 10
   var nodeId = ""
-  """{"dest":"n1","body":{"type":"init","node_id":"n1","node_ids":["n1"],"msg_id":1},"src":"c0","id":0}"""
   while (str != null) {
     System.err.println(s"Received $str")
     val message = parse(str).getOrElse(Json.Null)
     val cursor = message.hcursor
-    cursor.downField("body").downField("type").as[String].getOrElse("") match {
-      case "init" => {
-        val body = cursor.downField("body")
+    val body = cursor.downField("body")
+    body.downField("type").as[String].getOrElse("") match {
+      case "init" =>
+        """{"dest":"n1","body":{"type":"init","node_id":"n1","node_ids":["n1"],"msg_id":1},"src":"c0","id":0}"""
         nodeId = body.downField("node_id").as[String].getOrElse("")
-        val outputMessage: String = InitMessageResponse(nodeId, cursor.downField("src").as[String].getOrElse(""),
+        respond(InitMessageResponse(nodeId, cursor.downField("src").as[String].getOrElse(""),
           InitMessageResponseBody("init_ok", body.downField("msg_id").as[Int].getOrElse(0), messageId))
-          .asJson.noSpaces
-        System.err.println(s"Responding with $outputMessage")
-        System.out.println(outputMessage)
-        System.out.flush()
-      }
+          .asJson.noSpaces)
+      case "echo" =>
+        val echoMessage = body.downField("echo").as[String].getOrElse("")
+        respond(EchoMessageResponse(nodeId, cursor.downField("src").as[String].getOrElse(""),
+          EchoMessageResponseBody("echo_ok", body.downField("msg_id").as[Int].getOrElse(0), messageId, echoMessage))
+          .asJson.noSpaces)
     }
+    messageId += 1
     str = readLine()
+  }
+
+  private def respond(outputMessage: String): Unit = {
+    System.err.println(s"Responding with $outputMessage")
+    System.out.println(outputMessage)
+    System.out.flush()
   }
 }
