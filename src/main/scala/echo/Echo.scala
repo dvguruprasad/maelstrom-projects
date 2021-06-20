2795916f42f431e1aae61d66ed7bbc98579af4dd
Guruprasad Venkatesh <dv.guruprasad@gmail.com>
2021-06-20 09:27:50 +0530

    Refactor to use one message for init and echo

--- src/main/scala/echo/Echo.scala
+++ src/main/scala/echo/Echo.scala
@@ -1,6 +1,6 @@
 package echo
 
-import echo.messages.{InitMessageResponse, _}
+import echo.messages._
 import io.circe.parser.parse
 import io.circe.syntax.EncoderOps
 import io.circe.{ACursor, Json}
@@ -14,46 +14,58 @@ object Echo extends App {
 
   while (str != null) {
     System.err.println(s"Received $str")
-    val message = parse(str).getOrElse(Json.Null)
-    val cursor = message.hcursor
-
-    val body = cursor.downField("body")
-    body.downField("type").as[String].map {
-      case "init" =>
-        nodeId = asString(body, "node_id")
-        respond(
-          InitMessageResponse(
-            nodeId,
-            asString(cursor, "src"),
-            InitMessageResponseBody(
-              "init_ok",
-              body.downField("msg_id").as[Int].getOrElse(0),
-              messageId
-            )
-          ).asJson.noSpaces
-        )
-      case "echo" =>
-        val echoMessage = body.downField("echo").as[String].getOrElse("")
-        respond(
-          EchoMessageResponse(
-            nodeId,
-            cursor.downField("src").as[String].getOrElse(""),
-            EchoMessageResponseBody(
-              "echo_ok",
-              body.downField("msg_id").as[Int].getOrElse(0),
-              messageId,
-              echoMessage
-            )
-          ).asJson.noSpaces
-        )
-
+    processMessage(parse(str).getOrElse(Json.Null)) match {
+      case Right(response) => respond(response.asJson.noSpaces)
+      case Left(error) =>
+        System.err.println(s"Could not process message: $error")
     }
     messageId += 1
     str = readLine()
   }
 
-  private def asString(body: ACursor, node_id: String) =
-    body.downField(node_id).as[String].getOrElse("")
+  private def processMessage(message: Json) = {
+    val body = message.hcursor.downField("body")
+    for {
+      typeField <- body.downField("type").as[String]
+      sourceMessageId <- body
+        .downField("msg_id")
+        .as[Int]
+      src <- message.hcursor.downField("src").as[String]
+    } yield response(typeField, src, sourceMessageId, body)
+  }
+
+  def response(
+      `type`: String,
+      src: String,
+      sourceMessageId: Int,
+      body: ACursor
+    ): EchoMessage =
+    `type` match {
+      case "init" =>
+        nodeId = body.downField("node_id").as[String].getOrElse("")
+        EchoMessage(
+          nodeId,
+          src,
+          EchoMessageBody(
+            "init_ok",
+            Some(sourceMessageId),
+            messageId,
+            None
+          )
+        )
+
+      case "echo" =>
+        EchoMessage(
+          nodeId,
+          src,
+          EchoMessageBody(
+            "echo_ok",
+            Some(sourceMessageId),
+            messageId,
+            Some(body.downField("echo").as[String].getOrElse(""))
+          )
+        )
+    }
 
   private def respond(outputMessage: String): Unit = {
     System.err.println(s"Responding with $outputMessage")
--- src/main/scala/echo/messages/package.scala
+++ src/main/scala/echo/messages/package.scala
@@ -3,52 +3,15 @@ package echo
 import io.circe.generic.JsonCodec
 
 package object messages {
-  @JsonCodec case class InitMessage(
-      src: String,
-      dest: String,
-      body: InitMessageBody
-    )
-
-  @JsonCodec case class InitMessageBody(
-      `type`: String,
-      node_id: String,
-      msg_id: Int
-    )
-
-  @JsonCodec case class InitMessageResponse(
-      src: String,
-      dest: String,
-      body: InitMessageResponseBody
-    )
-
-  @JsonCodec case class InitMessageResponseBody(
-      `type`: String,
-      in_reply_to: Int,
-      msg_id: Int
-    )
-
-  @JsonCodec case class EchoMessage(
+  @JsonCodec sealed case class EchoMessage(
       src: String,
       dest: String,
       body: EchoMessageBody
     )
-
-  @JsonCodec case class EchoMessageBody(
+  @JsonCodec sealed case class EchoMessageBody(
       `type`: String,
-      echo: String,
-      msg_id: Int
-    )
-
-  @JsonCodec case class EchoMessageResponse(
-      src: String,
-      dest: String,
-      body: EchoMessageResponseBody
-    )
-
-  @JsonCodec case class EchoMessageResponseBody(
-      `type`: String,
-      in_reply_to: Int,
+      in_reply_to: Option[Int],
       msg_id: Int,
-      echo: String
+      echo: Option[String]
     )
 }
